4bd524ae1b9db05713423e2bf82930d690794272
harini <harinisiva1997@gmail.com>
2021-06-20 17:43:09 +0530

    linking between pages

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import React from 'react'
+import Link from 'next/link'
 function HomePage() {
-    return <h1>The Home Page</h1>
+    return <React.Fragment>
+        <h1>The Home Page</h1>
+        <ul>
+            <li>
+                <Link href="/news/next-js-is-a-great-framework">Next Js is a great framework</Link>
+            </li>
+            <li>
+                <Link href="/news/something-new">something new</Link>
+            </li>
+        </ul>
+    </React.Fragment>
 }
 
 export default HomePage
